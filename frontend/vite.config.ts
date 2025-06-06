2b5bbc5f2aff89c3408be970b956967eec74c331
Kavyaraj Mangrola <kavyarajmangrola16@gmail.com>
2025-06-07 00:31:57 +0530

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://todo-application-jnyh.onrender.com',
         changeOrigin: true,
         secure: false,
       },
